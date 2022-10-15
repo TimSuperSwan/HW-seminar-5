0e8b06aa2b565690258a30d200c93f1ef9dc8393
Artem <lebartigor@gmail.com>
2022-10-15 19:42:21 +0300

    решил первую задачу

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Семинар5дз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        //____________________ДОМШНЕЕ ЗАДАНИЕ 5 СЕМИНАР________________________
+
+        /*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+        Напишите программу, которая покажет количество чётных чисел в массиве.
+        [345, 897, 568, 234] -> 2 */
+
+        /*
+        Console.WriteLine("Введите размерность массива:");
+        int SizeArray = int.Parse(Console.ReadLine()!);
+        int[] Array = GetArray(SizeArray,100,999);
+        int count = 0;
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i]%2 == 0) count++;  
+        }
+        Console.WriteLine($"[{String.Join(", ", Array)}]");
+        Console.WriteLine($"В массиве {count} четных чисел");
+
+        int[] GetArray (int size, int minnum, int maxnum)
+        {
+            int[] res = new int[size];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = new Random().Next(minnum, maxnum+1);
+            }
+            return res;
+        }
+        
+        // Аж с первого раза с чистого листа никуда не подглядывая и даже без единой ошибки!!! Вау
+
+        */
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+            
+        }
+    }
+}
